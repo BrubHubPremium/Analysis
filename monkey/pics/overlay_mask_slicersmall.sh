f59d9d1bb660ec252e471a2198486a96f24a7c8a
brubhub <54552227+brubhub@users.noreply.github.com>
2019-09-03 17:10:52 -0400

    Create overlay_mask_slicersmall.sh

---
+++ monkey/pics/overlay_mask_slicersmall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# monkey
+# ex. input: site-uwo/sub-0032178
+monk=$1
+
+jdir=/data3/cdb/jcho/monkey
+picdir=${jdir}/maskpics
+rendered=${jdir}/maskpics/rendered
+
+site=`echo $monk | cut -d "/" -f1`
+subject=`echo $monk | cut -d "/" -f2`
+
+# anatimg=${jdir}/data/${site}/${subject}/anat_avg/${subject}_T1w_denoise_N4_anat_avg.nii.gz
+# maskimg=${jdir}/data/${site}/${subject}/anat_avg/${subject}_T1w_denoise_N4_anat_avg_brainmask_afni.nii.gz
+
+anatimg=${jdir}/data/${site}/${subject}/anat_avg/ants/${subject}_T1w_denoise_N4_anat_avg.nii.gz
+maskimg=${jdir}/data/${site}/${subject}/anat_avg/ants/${subject}_T1w_denoise_N4_anat_avg_mask.nii.gz
+
+overlay 1 1 ${anatimg} -a ${maskimg} 1 1 ${rendered}/ants/${site}_${subject}_rendered
+
+slicer ${rendered}/ants/${site}_${subject}_rendered -a ${picdir}/ants/${site}_${subject}_slicersmall.png

0285f1994cff8055e6811a13577bfe3582db23fe
brubhub <54552227+brubhub@users.noreply.github.com>
2019-09-03 17:47:18 -0400

    Update slicersmall_Xindi_output.sh

--- monkey/pics/slicersmall_Xindi_output.sh
+++ monkey/pics/slicersmall_Xindi_output.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 # monkey
-# ex. input:
+# example call: 
+# cd /Users/jaewook.cho/Downloads/xindi_sent/Others
+# MaskDir=O_site-other_PreMasks_T_site-all_ME_30
+# T1Dir=site-other/TestT1w
+# for i in `ls ${T1Dir}/*`
+# do
+# FileName=$(basename $i)
+# FilePrefix=${FileName%*.nii.gz}
+# anatimg=${i}
+# maskimg=${MaskDir}/${FilePrefix}_pre_mask.nii.gz
+# picdir=/Users/jaewook.cho/Downloads/xindi_sent/pics/${MaskDir}
+# bash /Users/jaewook.cho/slicersmall_Xindioutput.sh ${i} ${MaskDir}/${FilePrefix}_pre_mask.nii.gz /Users/jaewook.cho/Downloads/xindi_sent/pics/${MaskDir}
+# done
 
 anatimg=$1
 maskimg=$2
 picdir=$3
 mkdir -p ${picdir}/rendered
 
-sub=`echo ${anatimg} | cut -d '_' -f1`
+subject=`echo ${anatimg} | cut -d '_' -f1`
+subject=`echo ${subject} | cut -d '/' -f3`
 
 overlay 1 1 ${anatimg} -a ${maskimg} 1 1 ${picdir}/rendered/${subject}_rendered
 
